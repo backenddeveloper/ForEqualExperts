4ff4eaf287baef6ad71c3a8b091b2bce50168583
Graham Turner <grahamturnernew@hotmail.com>
2018-02-12 21:38:12 +0000

    Passing PEP8

--- features/steps/fizzBuzz.py
+++ features/steps/fizzBuzz.py
@@ -1,14 +1,16 @@
-from behave import given, then, when
-
+from behave import given, then
 from src.FizzBuzz import FizzBuzz
+
 
 @given(u'a {number:d} that divides by {divisor:d}')
 def step_impl(context, number, divisor):
     context.number = number
 
+
 @given(u'a {number:d} that doesn\'t divide by 2, 5 or 15')
 def step_impl(context, number):
     context.number = number
+
 
 @then(u'the output {output} is displayed')
 def step_impl(context, output):
--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -5,16 +5,15 @@ class FizzBuzz:
 
     @staticmethod
     def produce_output(number: int) -> str:
-        
+
         if number % 15 == 0:
             return 'FizzBuzz'
-        elif number % 3 ==0:
+        elif number % 3 == 0:
             return 'Fizz'
         elif number % 5 == 0:
             return 'Buzz'
         else:
             return str(number)
-
 
     @staticmethod
     def produce_sequence(infimum, supremum, produce_output=produce_output, range=range):
